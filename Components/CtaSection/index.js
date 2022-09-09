ab2cf182c6cee2a8d5a72a34eb5258e763ad7e28
Akshay <rathodak315@gmail.com>
2022-09-09 17:19:59 +0530

    added cta section

--- Components/CtaSection/index.js
+++ Components/CtaSection/index.js
@@ -7,6 +7,14 @@ const CtaSection = () => {
                 <div className="left-side-container">
                     <img src="/assets/Cta/IMAGE (11).png" alt="cta-img" />
                 </div>
+                <div className="right-side-container">
+                    <div className="heading">Start for free today</div>
+                    <div className="desc">Start using our services with all the advantages for 3 months for free. Our support will help with any questions. Start your development with us.</div>
+                    <div className="buttons-wrapper">
+                        <div className="contact-button">Contact Us</div>
+                        <div className="Pricing-button">View Pricing</div>
+                    </div>
+                </div>
             </div>
         </div>
     )
